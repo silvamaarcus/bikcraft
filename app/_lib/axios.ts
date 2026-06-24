b6c6ab0704bec313a01cbd5daf6e56eaeaea1853
Marcus Silva <devsilvamarcus@gmail.com>
2026-06-24 03:45:19 -0300

    feat: add .env para usar a variável de ambiente

--- app/_lib/axios.ts
+++ app/_lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
 export const viaCep = axios.create({
