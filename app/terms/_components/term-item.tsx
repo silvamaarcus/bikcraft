bb2bea3b969f18e38f609df2c7d815ea2c85a3c4
Marcus Silva <devsilvamarcus@gmail.com>
2026-06-24 02:06:06 -0300

    feat: add TermsPage

---
+++ app/terms/_components/term-item.tsx
@@ -0,0 +1,25 @@
+interface TermItemProps {
+  children: React.ReactNode;
+}
+
+interface TermItemTitleProps {
+  children: React.ReactNode;
+}
+
+interface TermItemParagraphProps {
+  children: React.ReactNode;
+}
+
+const TermItemTitle = ({ children }: TermItemTitleProps) => {
+  return <h2 className="mb-5 text-2xl font-semibold">{children}</h2>;
+};
+
+const TermItemParagraph = ({ children }: TermItemParagraphProps) => {
+  return <p className="mt-2 max-w-200 font-mono text-lg">{children}</p>;
+};
+
+const TermItem = ({ children }: TermItemProps) => {
+  return <div className="flex w-full flex-col">{children}</div>;
+};
+
+export { TermItem, TermItemParagraph, TermItemTitle };
---
+++ app/terms/page.tsx
@@ -0,0 +1,82 @@
+import TitleComponent from '../_components/title';
+import {
+  TermItem,
+  TermItemParagraph,
+  TermItemTitle,
+} from './_components/term-item';
+
+const TermsPage = () => {
+  return (
+    <section>
+      <div className="bg-foreground">
+        <div className="container py-15">
+          <TitleComponent
+            subtitle="Termos de uso"
+            title="termos e condições"
+            color="white"
+          />
+        </div>
+      </div>
+      <div>
+        <div className="container flex flex-col space-y-12 py-30">
+          <TermItem>
+            <TermItemTitle>1. Política de Estorno</TermItemTitle>
+            <TermItemParagraph>
+              1.1 Aceitamos cartões de crédito com as bandeiras: Visa,
+              Mastercard, ELO e American Express. Para pagamentos à vista também
+              aceitarmos PIX e Boleto através do PagSeguro.
+            </TermItemParagraph>
+            <TermItemParagraph>
+              1.2 Aceitamos cartões de crédito com as bandeiras: Visa,
+              Mastercard, ELO e American Express. Para pagamentos à vista também
+              aceitarmos PIX e Boleto através do PagSeguro.
+            </TermItemParagraph>
+            <TermItemParagraph>
+              1.3 Aceitamos cartões de crédito com as bandeiras: Visa,
+              Mastercard, ELO e American Express. Para pagamentos à vista também
+              aceitarmos PIX e Boleto através do PagSeguro.
+            </TermItemParagraph>
+          </TermItem>
+          <TermItem>
+            <TermItemTitle>2. Conta do Usuário</TermItemTitle>
+            <TermItemParagraph>
+              2.1 Aceitamos cartões de crédito com as bandeiras: Visa,
+              Mastercard, ELO e American Express. Para pagamentos à vista também
+              aceitarmos PIX e Boleto através do PagSeguro.
+            </TermItemParagraph>
+            <TermItemParagraph>
+              2.2 Aceitamos cartões de crédito com as bandeiras: Visa,
+              Mastercard, ELO e American Express. Para pagamentos à vista também
+              aceitarmos PIX e Boleto através do PagSeguro.
+            </TermItemParagraph>
+            <TermItemParagraph>
+              2.3 Aceitamos cartões de crédito com as bandeiras: Visa,
+              Mastercard, ELO e American Express. Para pagamentos à vista também
+              aceitarmos PIX e Boleto através do PagSeguro.
+            </TermItemParagraph>
+          </TermItem>
+          <TermItem>
+            <TermItemTitle>3. Seguro Bikcraft</TermItemTitle>
+            <TermItemParagraph>
+              3.1 Aceitamos cartões de crédito com as bandeiras: Visa,
+              Mastercard, ELO e American Express. Para pagamentos à vista também
+              aceitarmos PIX e Boleto através do PagSeguro.
+            </TermItemParagraph>
+            <TermItemParagraph>
+              3.2 Aceitamos cartões de crédito com as bandeiras: Visa,
+              Mastercard, ELO e American Express. Para pagamentos à vista também
+              aceitarmos PIX e Boleto através do PagSeguro.
+            </TermItemParagraph>
+            <TermItemParagraph>
+              3.3 Aceitamos cartões de crédito com as bandeiras: Visa,
+              Mastercard, ELO e American Express. Para pagamentos à vista também
+              aceitarmos PIX e Boleto através do PagSeguro.
+            </TermItemParagraph>
+          </TermItem>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default TermsPage;
